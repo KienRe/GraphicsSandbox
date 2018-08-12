bc20bcc24523fd8bcffc44c8aa7cc11d91a74a05
Kienauer Rene <rene102@live.de>
2018-08-12 22:10:03 +0200

    Simple Rectangle

--- HelloGraphics/Main.cpp
+++ HelloGraphics/Main.cpp
@@ -14,10 +14,18 @@ SDL_GLContext glContext;
 
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+float vertices[] =
+{
+	0.5f,  0.5f, 0.0f,		// top right
+	0.5f, -0.5f, 0.0f,		// bottom right
+	-0.5f, -0.5f, 0.0f,		// bottom left
+	-0.5f,  0.5f, 0.0f		// top left 
+};
+
+unsigned int indices[] =
+{
+	0, 1, 3,				// first triangle
+	1, 2, 3					// second triangle
 };
 
 const char* vertexShaderSource =
@@ -118,11 +126,19 @@ int main(int argc, char* args[])
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+	//Vertex Buffer Object Setup
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//Element Buffer Object Setup
+	unsigned int EBO;
+	glGenBuffers(0, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Vertex Attributes Setup
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -198,7 +214,7 @@ int main(int argc, char* args[])
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		ImGui::Render();
 		SDL_GL_SwapWindow(window);
