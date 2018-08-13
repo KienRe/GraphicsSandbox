a77446aa10583f23af6597003062352e23ae21b1
KienRe <rene102@live.de>
2018-08-13 12:04:42 +0200

    Textured Rectangle

--- HelloGraphics/Main.cpp
+++ HelloGraphics/Main.cpp
@@ -9,6 +9,8 @@
 #include <imgui.h>
 #include "imgui_impl_sdl_gl3.h"
 
+#include "stb_image.h"
+
 #include "Shader.h"
 
 SDL_Window* window;
@@ -20,10 +22,22 @@ bool drawWireframe = false;
 bool allowWireframeSwitch = false;
 
 float vertices[] = {
-	// positions			// colors
-	0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,   // bottom left
-	0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f    // top 
+	// positions          // colors           // texture coords
+	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,	// top right
+	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,	// bottom right
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+
+float texCoords[] = {
+	0.0f,0.0f,
+	1.0f,0.0f,
+	0.5f,1.0f
+};
+
+unsigned int indices[] = {
+	0, 1, 3, // first triangle
+	1, 2, 3  // second triangle
 };
 
 void ImguiFrame()
@@ -119,15 +133,86 @@ int main(int argc, char* args[])
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	//Element Buffer Object Setup
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//Vertex Attributes Setup
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	stbi_set_flip_vertically_on_load(true);
+
+	//Texture1
+	int width1, height1, nrChannels1;
+	unsigned char *data1 = stbi_load("res/textures/test1.png", &width1, &height1, &nrChannels1, 0);
+
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data1)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_RGB, GL_UNSIGNED_BYTE, data1);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		stbi_image_free(data1);
+	}
+	else
+	{
+		std::cout << "Failed to load Texture1" << std::endl;
+	}
+
+	//Texture2
+	int width2, height2, nrChannels2;
+	unsigned char *data2 = stbi_load("res/textures/test2.png", &width2, &height2, &nrChannels2, 0);
+
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	if (data2)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		stbi_image_free(data2);
+	}
+	else
+	{
+		std::cout << "Failed to load Texture2" << std::endl;
+	}
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
 
 	//Shader Setup
 	Shader ourShader("res/shaders/shader.vs", "res/shaders/shader.fs");
-
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
 
 	while (event.type != SDL_QUIT)
 	{
@@ -166,7 +251,7 @@ int main(int argc, char* args[])
 		ourShader.use();
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		ImGui::Render();
 		SDL_GL_SwapWindow(window);
